77a877ce7bfb45cc5c8bbe49cb89f0971a4cfb45
Jaechan Lee <ljae0724@gmail.com>
2024-04-20 21:37:12 -0400

    Transfer to RPI

--- opencv_livecam_test.py
+++ opencv_livecam_test.py
@@ -1,11 +1,19 @@
 import cv2
 import numpy as np
+from cv2 import dnn_superres
 
 # Connect to Theta V (port 3 on Jaechan's computer)
-capture = cv2.VideoCapture(3)
+capture = cv2.VideoCapture(2)
 if not capture.isOpened():
     print("Cannot open camera")
     exit()
+
+## Set up neural upscaling
+# Create an SR object
+#upsample_factor = 2
+#sr = dnn_superres.DnnSuperResImpl_create()
+#sr.readModel(r"C:\Users\Jaechan Lee\Desktop\JHU\__SP24\MultiD_AquariumProject\Upscaling\FSRCNN-small_x2.pb")
+#sr.setModel("fsrcnn", upsample_factor)
 
 shift = 0
 zoom = 0
@@ -18,7 +26,7 @@ while True:
     if not ret:
         print("Cannot receive frame")
         break
-    
+
     # Crop top and bottom of feed to filter warping
     cropped_frame = frame[v_margins:960-v_margins, 0:1920]    
     
@@ -33,21 +41,23 @@ while True:
     
     # Crop to ROI
     shift = shift%cropped_frame.shape[1]
-    print(shift)
     roi = padded_frame[zoom:new_height-zoom, ((0+shift)%(padded_frame.shape[1]))+int(zoom*ratio):((roi_x+shift)%(padded_frame.shape[1]))-int(zoom*ratio)]
 
     finalImage = cv2.resize(roi, (int(roi_x), int(roi_y)))
-
-    cv2.imshow('finalImage', finalImage)    
+    #upscaledImage = sr.upsample(finalImage, )
+    #upscaledImage = cv2.resize(upscaledImage, (int(roi_x), int(roi_y)))
+    cv2.imshow('finalImage', finalImage)
+    #cv2.imshow('upscaledImage', upscaledImage)    
+    
     key = cv2.waitKey(20) & 0xFF
 
     # Stop feed wwhen q hit
     if key == ord('q') or key == 27:
         break
     elif key == ord('a'):
-        shift -= 3
+        shift -= 10
     elif key == ord('d'):
-        shift += 3
+        shift += 10
     elif key == ord('w'):
         zoom += 10
     elif key == ord('s'):
@@ -55,8 +65,8 @@ while True:
         
     if zoom <= 0:
         zoom = 0
-    elif zoom >=100:
-        zoom = 100
+    elif zoom >=200:
+        zoom = 200
 
     
     
